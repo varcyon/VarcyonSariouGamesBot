f5bf6b9b5f7fbf95db61eae05b10e889b3c80dde
varcyon <darkenedmagi@gmail.com>
2021-11-15 14:55:39 -0600

    3

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<title>Varcyon Sariou Games </title>
+<link href= "https://fonts.googleapis.com/css?family=Coda" rel="stylesheet">
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+<link href="css/global.css" rel="stylesheet" type="text/css">
+<link href="css/home.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="js/jQuery.js"></script>
+</head>
+<body>
+<div class="loading-overlay"></div>
+<div class="loading-overlay-image-container">
+    <img src="assets/loading.gif" class="loading-overlay-img" />
+</div>
+
+<div class="content">
+    <div class="content-inner">
+        <div class="content-inner-padding">
+    
+         </div>
+    </div>  
+</div>
+</body>
+</html>
